32f6c4a2a64494d3e6507d407de4f19d97a4603c
Zalak <zalakthakkar12@gmail.com>
2025-12-22 21:10:10 +0530

    multer and cloudinary config

---
+++ src/middlewares/multer.middlewares.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+// this whole function saves the file on local server and returns localFilePath reqy=uired for uploading on cloudinary
+const storage = multer.diskStorage({
+  // we are using disk storage
+  destination: function (req, file, cb) {
+    // cb stands for call back
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
