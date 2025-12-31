0b67bf2293e783a2bd1e7c39f171c629076bac93
Zalak <zalakthakkar12@gmail.com>
2025-12-31 16:02:04 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logOutUser, refreshAccessToken, registerUser} from "../controllers/user.controllers.js";
+import {changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logOutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUsercoverImage} from "../controllers/user.controllers.js";
 import {upload} from '../middlewares/multer.middlewares.js'
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -24,5 +24,12 @@ router.route("/login").post(loginUser)
 // secured Routes
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)  // here we are not sending any data, we directly get the user so get is used
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) // here we want to update only some details not the whole so patch is used
+router.route("avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar )
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // data from params(url)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
